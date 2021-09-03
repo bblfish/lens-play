fffbcfc623653bd375e5762a2cd750ed1451d13b
Henry Story <henry.story@bblfish.net>
2021-09-03 13:33:42 +0200

    rename Resource to LDP

--- src/main/scala/server/Server.scala
+++ src/main/scala/server/Server.scala
@@ -32,38 +32,36 @@ object Server:
 
 	def now: Instant = Clock.systemUTC().instant()
 
-	/** Resources have content and metadata, which we illustrate with `created` field.
-	 * Resource is a recursive datatype that allows us to model a nested Web Server
-	 * resource hierarchy.
+	/** LDP stands for [[https://www.w3.org/TR/ldp/ Linked Data Platform]]
+	 *  This is a simplified model.
+	 *
+	 *  Resources have content and metadata, which we illustrate with `created` field.
+	 *  Resource is a recursive datatype that allows us to model a nested Web Server
+	 *  resource hierarchy.
 	 *
 	 * Locating a resource is just a matter of following the Map hierarchy deeper
 	 * and deeper inwards.
 	 * */
-	enum Resource:
-		case Container(content: Map[String, Resource] = Map(), created: Instant = now)
-		//we currently only have a TextResource, but we could add a mime type and have
-		//the content as a binary string to cover all cases
+	enum LDP:
+		case Container(content: Map[String, LDP] = Map(), created: Instant = now)
+
+		/* we currently only have a TextResource, but we could add a mime type and have
+		the content as a binary string to cover all cases */
 		case TextResource(content: String, created: Instant = now)
 
 		def summary: String = this match
 			case Container(content, _) => content.keys.mkString("- ", "\n- ", "\n")
 			case TextResource(content, _) => content
-	end Resource
+	end LDP
 
-	/**
-	 * We can think of a web server as just a root container with a number of resources
+	/** We can think of a web server as just a root container with a number of resources
 	 * (which can themselves be containers), each named by a key of type String.
-	 * A path of such strings gives a URL Path.
-	 */
-	val root = Resource.Container()
+	 * A path of such strings gives a URL Path. */
+	val root = LDP.Container()
 
 	object LDPC
 
 	case class Response(code: Int, content: String)
-
-	//	val contents = Lens[Container, Map[String, Resource[_]]](_.content){ newmap =>
-	//		cont => cont.copy(content = newmap)
-	//	}
 
 	type Path = List[String]
 
@@ -71,50 +69,46 @@ object Server:
 	 * typeclass for a basic Index on a Container, locates it's positing in the
 	 * `content` Map.
 	 */
-	given Index[Resource, String, Resource] =
-		Index(key => Focus[Resource](_.as[Resource.Container].content.index(key)))
+	given Index[LDP, String, LDP] =
+		Index(key => Focus[LDP](_.as[LDP.Container].content.index(key)))
 
-	/**
-	 * typeclass for Index on a Container type S given a List of indexes.
+	/** typeclass for Index on a Container type S given a List of indexes.
 	 * This is defined recursively.
-	 * thanks to Ken Scambler
-	 */
+	 * thanks to Ken Scambler */
 	given [S, I](using Index[S, I, S]): Index[S, List[I], S] =
 		Index {
 			case Nil => Focus[S]()
 			case key :: keys => Focus[S](_.index(key).index(keys))
 		}
 
-	/**
-	 * The HTTP methods that one can use to interact with a Container.
+	/** The HTTP methods that one can use to interact with a Container.
 	 * See local <src/test/scala/server/ServerTests.scala> for usage.
 	 * Docs to look at:
-	 * [[https://www.optics.dev/Monocle/docs/focus monocle focus]]
-	 */
-	extension (server: Resource)(using Index[Resource, List[String], Resource])
+	 * [[https://www.optics.dev/Monocle/docs/focus monocle focus]]  */
+	extension (server: LDP)(using Index[LDP, List[String], LDP])
 		def GET(path: List[String]): Response =
 			server.focus(_.index(path)).getOption match
 			case Some(res) => Response(200, res.summary)
 			case None => Response(404, "Content could not be found")
 
-
 		def POST(path: List[String])(
 			slug: String, newcontent: String | LDPC.type
-		): (Resource, Response) =
+		): (LDP, Response) =
 			//we limit to Containers, though a POST to a resource would work if its content is a monoid
-			val newCntr: Option[Resource] = server.focus(_.index(path).as[Resource.Container])
-				.modifyOption { (cntr: Resource.Container) =>
-					val index: Map[String, Resource] = cntr.content
+			val newCntr: Option[LDP] = server.focus(_.index(path).as[LDP.Container])
+				.modifyOption { (cntr: LDP.Container) =>
+					val index: Map[String, LDP] = cntr.content
 					val name = if index.get(slug).isEmpty then slug else s"${slug}_${counter.next()}"
 					val newRes = newcontent match
-						case LDPC => Resource.Container()
-						case text: String => Resource.TextResource(text)
+						case LDPC => LDP.Container()
+						case text: String => LDP.TextResource(text)
 					cntr.copy(content = index.updated(name, newRes))
 				}
 			newCntr match
-			case Some(newC: Resource.Container) => (newC, Response(200, "how do we pass the name of the new resource here?"))
+			case Some(newC: LDP.Container) => (newC, Response(200, "how do we pass the name of the new resource here?"))
 			case Some(resource) => (server, Response(500, "internatl server erorr. Seem to be replacing root container with a resource"))
 			case None => (server, Response(404, "container does not exist"))
+
 end Server
 
 
--- src/main/scala/server/test.sc
+++ src/main/scala/server/test.sc
@@ -4,8 +4,8 @@ import cats.data.NonEmptyList as NEL
 import server.Server
 import Server.{given,*}
 
-val Some(r1) = root.focus(_.index(Nil).as[Resource.Container]).modifyOption{ (cntr: Resource.Container) =>
-	val newMap = cntr.content.updated("hello",Resource.TextResource("hello"))
+val Some(r1) = root.focus(_.index(Nil).as[LDP.Container]).modifyOption{ (cntr: LDP.Container) =>
+	val newMap = cntr.content.updated("hello",LDP.TextResource("hello"))
 	cntr.copy(content=newMap)
 }
 
